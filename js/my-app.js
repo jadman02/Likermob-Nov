f1d1d36a4c0bcb977d74fb73422ee836f844ba60
jadman02 <jadman02@hotmail.com>
2014-11-11 11:45:11 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -213,7 +213,7 @@ var track_click = 0;
      
 $$.getJSON('http://www.smilesavers.net.au/'+ domain +'.php?callback=?', ''+ data_send +'',function(response){
 var itemlist = [];
-for (i = 0; i < response.length; i++) {itemlist.push({marker: response[i][0],title: response[i][3],page_id: response[i][1],post_id: response[i][2],photo_created: response[i][21],like: response[i][17],dislike: response[i][18],expiry: response[i][6]}); 
+for (i = 0; i < 5; i++) {itemlist.push({marker: response[i][0],title: response[i][3],page_id: response[i][1],post_id: response[i][2],photo_created: response[i][21],like: response[i][17],dislike: response[i][18],expiry: response[i][6]}); 
  
 var str = response[i][4];
 var singlequote = str.replace(/'/g, "qqqq");
