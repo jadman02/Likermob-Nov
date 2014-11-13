b12d8c0fd2df06cc37ae153ee17f329be12d7e87
jadman02 <jadman02@hotmail.com>
2014-11-13 12:22:28 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -8,8 +8,8 @@ var $$ = Dom7;
 
 myApp.onPageBeforeInit('index', function (page) {
 
-var uid = localStorage.getItem("uid");
-document.getElementById("profilepic").innerHTML = '<img src="http://graph.facebook.com/' + uid + '/picture?type=normal" style="margin:0 auto;text-align:center;width:80px;border-radius:50%;"/>';
+//var uid = localStorage.getItem("uid");
+//document.getElementById("profilepic").innerHTML = '<img src="http://graph.facebook.com/' + uid + '/picture?type=normal" style="margin:0 auto;text-align:center;width:80px;border-radius:50%;"/>';
 
     functionEmpty();
 
