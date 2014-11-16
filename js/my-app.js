5ae8199476dc153dc4392a66951ed435c5710680
jadman02 <jadman02@hotmail.com>
2014-11-16 11:49:03 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -280,8 +280,10 @@ if (type=='like') {$$( '#result' ).append('<li class="swipeout s_'+ response[i][
 }
 
 
-var $target = $$('page-content-scroll'); 
-$target.animate({scrollTop: $target.height()}, 50); 
+
+$$( ".page-content-scroll" ).scrollTop( 300 );
+
+
 
 if ((post_id_list instanceof Array) && (pages_list=='b')) {
 	
