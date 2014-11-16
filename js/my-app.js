0b517e7ca56b8c1f0dd5416dc2e683bdbd7e6fc2
jadman02 <jadman02@hotmail.com>
2014-11-16 17:09:22 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -317,6 +317,10 @@ $$( '#add_container' ).append('<li class="swipeout s_'+ response[i][2] +'"><img 
 
 
 
+$$("#loader-container").hide();
+$$('.page-content').scrollTop(294, 300);
+$$( ".page-content" ).addClass( "page-content-scroll" );
+
 
 }
 
@@ -328,9 +332,7 @@ $$( '#add_container' ).append('<li class="swipeout s_'+ response[i][2] +'"><img 
 // $$('.page-content').scrollTop($$('.full').offset().top - 44, 300);
 
 
-$$("#loader-container").hide();
-$$('.page-content').scrollTop(294, 300);
-$$( ".page-content" ).addClass( "page-content-scroll" );
+
 
 });
 
