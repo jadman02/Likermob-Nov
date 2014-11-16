cfa328edb3c03201f5f875749bdd7f76c5bc1944
jadman02 <jadman02@hotmail.com>
2014-11-16 15:22:34 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -324,7 +324,7 @@ $$("#loader-container").hide();
 
 var number_pages = localStorage.getItem("total_pages");
 var stop = Math.ceil(number_pages / 5);
-
+alert('stope:'+stop + 'position:' + position);
 if (position >= (stop-1)) {alert('at the end');}
 else{$$( ".page-content" ).addClass( "page-content-scroll" );}
 
