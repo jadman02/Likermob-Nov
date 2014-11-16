10f0e6adc4b0409ba7ee0fb0fd51c99f31edbc19
jadman02 <jadman02@hotmail.com>
2014-11-16 11:25:03 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -51,6 +51,7 @@ $$('.page-content-scroll').on('scroll', function (e) {
             $$("#result img" ).remove();
             if(number_of_scrolls >= 3) {$$( "#result li:nth-child(-n+5)" ).remove();}
             functionEmpty();
+            window.scrollTo(300,0);
         }
 
     }
