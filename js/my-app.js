2f6bb0a3617496d719f367971d20673e50468818
jadman02 <jadman02@hotmail.com>
2014-11-18 08:50:57 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -277,9 +277,9 @@ var stop = Math.ceil(response.length / 5);
 //var num = 12.1542;
 //alert(num % 1);
 
+   
 
-
-if (position < (stop-1)) {$$( '#result' ).append('<li id="listmarker"></li>');document.getElementById("listmarker").innerHTML = 'Items: ' + last;$$( ".page-content" ).removeClass( "page-content-scroll" );}
+if (position < (stop-1)) {$$( '#result' ).append('<li id="listmarker"></li>');document.getElementById("listmarker").innerHTML = '<div class="item-input"><div class="range-slider"><input type="range" min="0" max="100" step="0.1"></div></div>';$$( ".page-content" ).removeClass( "page-content-scroll" );}
 if (position == (stop-1)) {finish = last;$$( '#result' ).append('<li id="listmarker_end"></li>');$$( ".page-content" ).removeClass( "page-content-scroll" );}
 if (position > (stop-1)) {alert('no more');}
 
