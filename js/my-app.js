b772e669b05faf2fbc7bf1189e1170dbe2c8afd3
jadman02 <jadman02@hotmail.com>
2014-11-19 23:48:00 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -258,7 +258,7 @@ if (type=='like') {$$( '#result' ).append('<li class="swipeout full s_'+ respons
 }
 
 
-$$( '.swipeout' ).css( 'height', '25%' );
+$$( '.swipeout' ).css( 'height', '50%' );
 
 
 
