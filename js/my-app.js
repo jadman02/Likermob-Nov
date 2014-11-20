e53c82a76a2a88e017860d77d4e740f085acce90
jadman02 <jadman02@hotmail.com>
2014-11-20 11:00:47 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -576,7 +576,7 @@ function closeButton(post_id){
 var uid = localStorage.getItem("uid");
 $$.getJSON('http://www.smilesavers.net.au/dislike.php?callback=?','user_id=' + uid + '&post_id=' + post_id + '&type=dislike',function(res){
     
-    alert('Your name is '+res.fullname);
+   // alert('Your name is '+res.fullname);
 });
 	
 	
