666bde9b9276690622690043b7587a6ea7dc4174
jadman02 <jadman02@hotmail.com>
2014-11-22 13:57:57 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -288,7 +288,7 @@ $$("#loader-container").hide();
 if (position < (stop-1)) {$$( ".page-content" ).addClass( "page-content-scroll" );$$( "#result li:nth-child(-n+5)" ).remove();$$( '#loaded' ).css( 'width', percentage+'%' );$$( '#notloaded' ).css( 'width', minuspercentage+'%' );}
 if (position == (stop-1)) {$$( ".page-content" ).removeClass( "page-content-scroll" );$$('.page-content').scrollTop($$('#listmarker_end').offset().top-44, 300);$$( '#loaded' ).css( 'width', percentage+'%' );$$( '#notloaded' ).css( 'width', minuspercentage+'%' );}
 
-var swipeoutheight = $$('.swipeout:first').height();
+var swipeoutheight = $$(window).height();
 alert(swipeoutheight);
 $$( '.swipeout' ).css( 'height',  '800px' );
 
