e22ec6c27fb13cd4b7402e4574e049965d14a99d
jadman02 <jadman02@hotmail.com>
2014-11-23 10:04:38 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -14,7 +14,7 @@ myApp.onPageBeforeInit('index', function (page) {
 
 var uid = localStorage.getItem("uid");
 document.getElementById("profilepic").innerHTML = '<img src="http://graph.facebook.com/' + uid + '/picture?type=normal" style="margin:0 auto;text-align:center;width:80px;border-radius:50%;"/>';
-    functionEmpty('a',0,10,1);
+    functionEmpty('a',0,3,0);
 
 
 
@@ -104,7 +104,7 @@ ptrContent.on('refresh', function (e) {
   //  setTimeout(function () {
 $$("#result li").remove();
 $$("#loader-container").show();
-    functionEmpty('a',0,3,1);
+    functionEmpty('a',0,3,0);
         // When loading done, we need to reset it
         myApp.pullToRefreshDone();
  //   }, 500);
