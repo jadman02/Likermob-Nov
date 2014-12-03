b596c67fc137fd72828cbd3c321c2474a7a81ce6
jadman02 <jadman02@hotmail.com>
2014-12-03 20:00:38 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -256,6 +256,9 @@ if (type=='like') {$$( '#result' ).append('<li class="swipeout full s_'+ respons
 '</div>'+
 '<div class="swipeout-content">'+
 '<div class="item-content">'+
+ '<div class="item-media">'+
+                   '<img onclick="popUp(\''+ response[i][3]  +'\',\''+ singlequote  +'\',\''+ response[i][1]  +'\',\''+ response[i][2]  +'\')" src="http://smilesavers.net.au/images/compressed/'+response[i][1]+'_'+response[i][21]+'.jpg" style="width:100%;"/>'+
+               ' </div>'+
 '<div class="item-inner" style="border-bottom:0;">'+
 '<div class="item-title-row">'+
 '<div class="item-title"></div>'+
