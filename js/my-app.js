cdd06ebda536569047eace4b85cf8090a07b7db1
jadman02 <jadman02@hotmail.com>
2014-12-06 10:46:38 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -1735,7 +1735,7 @@ function moveHomeimage(){
      img.onload = function () {
      	windowsize = $$(window).width();
 	height_image = ((img.height/img.width) * windowsize)-60;
-     $$( '#resultlist' ).css( 'padding-top', height_image + 'px' );
+     $$( '.swipeout-content' ).css( 'padding-top', height_image + 'px' );
  };
 }
 
