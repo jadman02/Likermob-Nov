dfc6ca7b436c9386a69ce74e076e347b44b66e58
jadman02 <jadman02@hotmail.com>
2014-12-05 11:08:21 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -2034,6 +2034,7 @@ function openBrowser(){
 	var myPhotoBrowserStandalone = myApp.photoBrowser({
     navbarTemplate:'<div class="navbar"><div class="navbar-inner"><div class="left sliding"><a href="#" class="link close-popup photo-browser-close-link"><i class="icon icon-back"></i><span>Closezzz</span></a></div><div class="center sliding"><span class="photo-browser-current"></span> <span class="photo-browser-of">of</span> <span class="photo-browser-total"></span></div><div class="right" onclick="modalModal()">modal</div></div></div>',
     lazyLoadingInPrevNext:true,
+    expositionHideCaptions:true,
 photos : [
         {
             url: 'http://lorempixel.com/1024/1024/sports/1/',
