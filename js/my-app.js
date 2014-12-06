22dc4d928b0d06ca4fa520b91f37e4bb1c24cb6f
jadman02 <jadman02@hotmail.com>
2014-12-07 09:02:30 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -265,7 +265,7 @@ if (type=='like') {$$( '#result' ).append('<li class="swipeout full s_'+ respons
 
 '<div class="imagediv" onclick="openBrowser()" style="margin:0 auto;">'+
  '<div style="float:left;width:33.33%;">'+
-    '<a href="#" class="button button-red" style="margin:0 auto;border-radius:50%;width:50px;height:50px;background-color:#3b5998;"><i class="pe-7s-like2 pe-2x" style="color:white"></i></a>'+
+    '<a href="#" class="button button-red" style="margin:0 auto;border-radius:50%;border:0;width:50px;height:50px;background-color:#3b5998;"><i class="pe-7s-like2 pe-2x" style="color:white;margin-top:10px;;"></i></a>'+
   '</div>'+
   
    '<div style="float:left;width:33.33%;">'+
@@ -273,7 +273,7 @@ if (type=='like') {$$( '#result' ).append('<li class="swipeout full s_'+ respons
   '</div>'+
 
  ' <div style="float:left;width:33.33%;">'+
-   ' <a href="#" class="button button-green" style="margin:0 auto;border-radius:50%;width:50px;height:50px;background-color:#ff8000;"><i class="pe-7s-like2 pe-2x pe-rotate-180" style="color:white"></i></a>'+
+   ' <a href="#" class="button button-green" style="margin:0 auto;border-radius:50%;border:0;width:50px;height:50px;background-color:#ff8000;"><i class="pe-7s-like2 pe-2x pe-rotate-180" style="color:white;margin-top:10px;;"></i></a>'+
   '</div>'+
 
 
