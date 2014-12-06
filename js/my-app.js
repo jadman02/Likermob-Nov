db052340dc074708fa1a51c267b3828168cc7c65
jadman02 <jadman02@hotmail.com>
2014-12-06 22:58:41 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -1759,7 +1759,7 @@ function moveHomeimage(){
 	var positionimage = height_image - 80;
 	var liHeight = $$( "#content-block" ).height();
 
-	var resultheight = liHeight + height_image + 70;
+	var resultheight = liHeight + height_image + 55;
      $$( '.imagediv' ).css( 'padding-top', positionimage + 'px' );
      $$( '#result' ).css( 'height', resultheight + 'px' );
  };
