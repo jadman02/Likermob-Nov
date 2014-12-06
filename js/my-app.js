2fbe8bf7da04fcebd61ec3da6bafdc2c958c5a0a
jadman02 <jadman02@hotmail.com>
2014-12-07 09:22:10 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -265,15 +265,15 @@ if (type=='like') {$$( '#result' ).append('<li class="swipeout full s_'+ respons
 
 '<div class="imagediv" onclick="openBrowser()" style="margin:0 auto;">'+
  '<div style="float:left;width:33.33%;">'+
-    '<a href="#" class="button button-red" style="margin:0 auto;border-radius:50%;border:0;width:40px;height:40px;background-color:#3b5998;"><i class="pe-7s-like2 pe-2x" style="color:white;margin-top:10px;;"></i></a>'+
+    '<a href="#" class="button button-red" style="margin:0 auto;border-radius:50%;border:0;width:40px;height:40px;background-color:#3b5998;"><i class="pe-7s-like2 pe-2x" style="color:white;margin-top:5px;;"></i></a>'+
   '</div>'+
   
    '<div style="float:left;width:33.33%;">'+
-    '<a href="#" style="height:40px;width:40px;border-radius:50%;margin:0 auto;" class="button" onclick="getBusiness(\''+ response[i][1]  +'\');"><img src="http://graph.facebook.com/'+response[i][1]+'/picture?width=50&height=50" style="border-radius:50%;margin-left:-10px;"/></a>'+
+    '<a href="#" style="height:40px;width:40px;border-radius:50%;margin:0 auto;" class="button" onclick="getBusiness(\''+ response[i][1]  +'\');"><img src="http://graph.facebook.com/'+response[i][1]+'/picture?width=40&height=40" style="border-radius:50%;margin-left:-5px;"/></a>'+
   '</div>'+
 
  ' <div style="float:left;width:33.33%;">'+
-   ' <a href="#" class="button button-green" style="margin:0 auto;border-radius:50%;border:0;width:40px;height:40px;background-color:#ff8000;"><i class="pe-7s-like2 pe-2x pe-rotate-180" style="color:white;margin-top:10px;;"></i></a>'+
+   ' <a href="#" class="button button-green" style="margin:0 auto;border-radius:50%;border:0;width:40px;height:40px;background-color:#ff8000;"><i class="pe-7s-like2 pe-2x pe-rotate-180" style="color:white;margin-top:5px;"></i></a>'+
   '</div>'+
 
 
