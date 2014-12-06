45e832dedb1750736a8f2f52b41859d6cc747dfa
jadman02 <jadman02@hotmail.com>
2014-12-06 12:11:40 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -628,9 +628,6 @@ $$.getJSON('http://www.smilesavers.net.au/dislike.php?callback=?','user_id=' + u
 var position = localStorage.getItem("position");
 
  position++;
-$$( '.homecontent' ).css( 'background-image', 'url("http://smilesavers.net.au/images/cover.png")');
-$$( '.homecontent' ).css( 'background-size', '100%');
-$$( '.homecontent' ).css( 'background-repeat', 'no-repeat');}
 functionEmpty('a',0,1,position);
 
 //var start; 
