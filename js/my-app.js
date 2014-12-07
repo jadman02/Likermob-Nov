5b248941f1c5cb45cc5246beae20a44436645893
jadman02 <jadman02@hotmail.com>
2014-12-07 21:34:08 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -14,6 +14,8 @@ myApp.onPageBeforeInit('index', function (page) {
 
 var uid = localStorage.getItem("uid");
 document.getElementById("profilepic").innerHTML = '<img src="http://graph.facebook.com/' + uid + '/picture?type=normal" style="margin:0 auto;text-align:center;width:80px;border-radius:50%;"/>';
+document.getElementById("commentpic").innerHTML = '<img src="http://graph.facebook.com/' + uid + '/picture?type=normal" style="margin:0 auto;text-align:center;width:30px;border-radius:50%;"/>';
+
     functionEmpty('a',0,2,0);
 
 
