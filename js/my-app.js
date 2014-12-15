c861d38088f8b6d77c90ffb4184d49ebaf1647c9
jadman02 <jadman02@hotmail.com>
2014-12-16 09:54:08 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -268,12 +268,12 @@ if (type=='like') {$$( '#result' ).append('<li class="swipeout full s_'+ respons
 
 '<div class="imagediv" onclick="openBrowser();makeComment();" style="margin:0 auto;">'+
 
-  '<a href="#" style="height:60px;width:60px;border-radius:50%;margin:0 auto;background-color:hsla(0, 0%, 100%, 0.5);" class="button" onclick="getBusiness(\''+ response[i][1]  +'\');"><img src="http://graph.facebook.com/'+response[i][1]+'/picture?width=40&height=40" style="border-radius:50%;margin-left:-10px;"/></a>'+
+  '<a href="#" style="height:60px;width:60px;border-radius:50%;margin:0 auto;background-color:hsla(0, 0%, 100%, 0.5);" class="button"><i class="pe-7s-comment pe-lg"></i></a>'+
 
     
   
-
-
+//onclick="getBusiness(\''+ response[i][1]  +'\');"
+//<img src="http://graph.facebook.com/'+response[i][1]+'/picture?width=40&height=40" style="border-radius:50%;margin-left:-10px;"/>
 //onclick="popUp(\''+ response[i][3]  +'\',\''+ singlequote  +'\',\''+ response[i][1]  +'\',\''+ response[i][2]  +'\')"
 '</div>'+
 '<div class="swipeout-content">'+
