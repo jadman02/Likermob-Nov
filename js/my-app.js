50bf6098fa9838b6ffc3c3b31b8c49854e8e3c92
jadman02 <jadman02@hotmail.com>
2014-12-16 11:42:00 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -270,7 +270,7 @@ if (type=='like') {$$( '#result' ).append('<li class="swipeout full s_'+ respons
 '<a href="#" onclick="makeComment();" style="z-index:1000000;float:right;margin-right:5px;height:40px;width:40px;border-radius:50%;background-color:white;" class="button"><i class="pe-7s-more pe-2x" style="margin-left:-5px;margin-top:5px;"></i></a>'+
 '<a href="#" onclick="makeComment();" style="z-index:1000000;float:right;margin-right:5px;height:40px;width:40px;border-radius:50%;background-color:white;" class="button"><i class="pe-7s-comment pe-2x" style="margin-left:-2px;margin-top:5px;"></i></a>'+
   '<a href="#" onclick="getBusiness(\''+ response[i][1]  +'\');" style="z-index:1000000;float:right;margin-right:5px;height:40px;width:40px;border-radius:50%;background-color:hsla(0, 0%, 100%, 0.5);" class="button"><img src="http://graph.facebook.com/'+response[i][1]+'/picture?width=40&height=40" style="border-radius:50%;margin-left:-10px;"/></a>'+
-
+'<div style="float:right;font-size:14px;clear:both;">Uploaded by Smilesavers 6 minutes ago</div>'+
 
 
 //openBrowser();
