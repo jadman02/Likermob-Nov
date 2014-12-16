1b6dd18f6c4c3726d2c76b5d1ae0e33b64117f3e
jadman02 <jadman02@hotmail.com>
2014-12-16 11:18:27 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -292,7 +292,7 @@ if (type=='like') {$$( '#result' ).append('<li class="swipeout full s_'+ respons
 '<div class="item-subtitle hide">by '+response[i][16]+'</div>'+
 '</div>'+
 '</a>'+
-   '<div class="content-block" id="content-block" style="margin-top:10px;clear:both;">'+
+   '<div class="content-block" id="content-block" style="margin-top:20px;clear:both;">'+
      ' <div class="content-block-inner" style="padding-bottom:41px;background-color:hsla(0, 0%, 100%, 0.5)">'+
 
      '<div id="dealtitle" style="float:left;"><b>'+ response[i][3] + '</b></div><br/><div>'+response[i][4]+'</div>'+
