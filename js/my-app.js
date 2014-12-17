f04b2b33dc6f573b012d4746f26be2b45cb03cae
jadman02 <jadman02@hotmail.com>
2014-12-17 17:09:00 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -299,11 +299,11 @@ if (type=='like') {$$( '#result' ).append('<li class="swipeout full s_'+ respons
 
          '<div style="float:left;font-size:14px;clear:both;"><i class="pe-7s-clock pe-lg"></i> Expires in 6 days</div>'+
          
-'<div style="float:right;font-size:14px;"><i class="pe-7s-like2 pe-lg button" style="margin-right:2px;color:#3b5998"></i>'+ response[i][17] + '<i class="pe-7s-like2 pe-lg pe-rotate-180 button" style="color:#ff8000;margin-left:5px;margin-right:2px;"></i>'+ response[i][18] + '</div>'+
+'<div style="float:right;font-size:14px;"><i class="pe-7s-like2 pe-lg button" style="border-radius:50%;border-color:#3b5998;margin-right:2px;color:#3b5998"></i>'+ response[i][17] + '<i class="pe-7s-like2 pe-lg pe-rotate-180 button" style="border-radius:50%;color:#ff8000;border-color:#ff8000;margin-left:5px;margin-right:2px;"></i>'+ response[i][18] + '</div>'+
 
      '</div>'+
-
-
+'<a href="#" class="button" onclick="getComments()">Get Comments</a>'+
+'<div id="commentlist"></div>'
 
     '</div>'+
 
