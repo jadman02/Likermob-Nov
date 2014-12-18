b2e0d90493933bfda5b0bfdb44ad8de507888956
jadman02 <jadman02@hotmail.com>
2014-12-18 17:36:31 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -304,7 +304,9 @@ if (type=='like') {$$( '#result' ).append('<li class="swipeout full s_'+ respons
          '<div style="float:left;font-size:14px;clear:both;"><i class="pe-7s-clock pe-lg"></i> Expires in 6 days</div>'+
 
          
-'<div style="float:right;font-size:14px;"><a href="#" class="button" style="float:left;border-radius:50%;border-color:#3b5998;width:30px;height:30px;"><i class="pe-7s-like2 pe-lg" style="margin-right:2px;color:#3b5998"></i></a><a href="#" class="button" style="float:left;border-radius:50%;border-color:#ff8000;width:30px;height:30px;"><i class="pe-7s-like2 pe-lg pe-rotate-180 button" style="color:#ff8000;margin-left:5px;margin-right:2px;"></i></a></div>'+
+'<div style="float:right;font-size:14px;"><a href="#" class="button" style="float:left;border-radius:50%;border-color:#3b5998;width:30px;height:30px;"><i class="pe-7s-like2 pe-lg" style="margin-right:2px;color:#3b5998"></i></a><span class="badge">'+ response[i][17] + '</span><a href="#" class="button" style="float:left;border-radius:50%;border-color:#ff8000;width:30px;height:30px;"><i class="pe-7s-like2 pe-lg pe-rotate-180" style="color:#ff8000;margin-left:5px;margin-right:2px;"></i></a><span class="badge">'+ response[i][18] + '</span></div>'+
+
+
 
      '</div>'+
 
