8379c84f121e2be7a94b107cd7bd7485609161dc
jadman02 <jadman02@hotmail.com>
2014-12-22 00:41:27 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -31,7 +31,7 @@ $$('.pull-to-refresh-content').on('scroll', function (e) {
         
             
         }
-    if (pageScroll == -0.5) {$$( '.homecontent' ).css( 'background-size', '100%');$$( '.homecontent' ).css( 'background-position', '0% 0%');}
+    if (pageScroll == 0) {$$( '.homecontent' ).css( 'background-size', '100%');$$( '.homecontent' ).css( 'background-position', '0% 0%');}
    
    
     
