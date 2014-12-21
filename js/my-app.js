469ee0fa164a5ef3941b3c3750ef2db15f8f6a71
jadman02 <jadman02@hotmail.com>
2014-12-21 23:11:37 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -23,10 +23,11 @@ $$('.pull-to-refresh-content').on('scroll', function (e) {
 
     var pageContent = this;
     var pageScroll = pageContent.scrollTop;
+  var positionpageScroll = pageScroll * -1;
   var background_size = (pageScroll * -1) + 100;
    if (pageScroll < 0) {
         $$( '.homecontent' ).css( 'background-size', background_size + '%');
-        $$( '.homecontent' ).css( 'background-position', pageScroll + '% 0%');
+        $$( '.homecontent' ).css( 'background-position', positionpageScroll + '% 0%');
             
         }
     else {$$( '.homecontent' ).css( 'background-size', background_size + '100%');$$( '.homecontent' ).css( 'background-position', '0% 0%');}
