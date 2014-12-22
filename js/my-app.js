568526b94265b78b889ffcd1bbdf6b0a943910a3
jadman02 <jadman02@hotmail.com>
2014-12-22 16:02:34 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -254,13 +254,15 @@ for (i = start; i < finish; i++) {
 var favEntries = JSON.parse(localStorage.getItem("favEntries"));
 var star; 
 
-
+if (typeof favEntries === 'undefined') {star = '<a href="#" onclick="favList(\''+ response[i][1]  +'\')" style="z-index:100;float:right;margin-right:5px;height:40px;width:40px;border:0;background-color:transparent;" class="button"><i class="pe-7s-star pe-2x" style="margin-left:-2px;margin-top:5px;"></i></a>';}
+else {
 for (i = 0; i < favEntries.length; i++) {        
-if (favEntries[i].page_id == response[i][1]){star = '<a href="#" onclick="removefavList(\''+ response[i][1]  +'\')" style="z-index:100;float:right;margin-right:5px;height:40px;width:40px;border:0;background-color:transparent;" class="button"><i class="pe-7s-star pe-2x" style="margin-left:-2px;margin-top:5px;color:#ffcc00;"></i></a>';alert('you like this');}
+if (favEntries[i].page_id == response[i][1]){star = '<a href="#" onclick="removefavList(\''+ response[i][1]  +'\')" style="z-index:100;float:right;margin-right:5px;height:40px;width:40px;border:0;background-color:transparent;" class="button"><i class="pe-7s-star pe-2x" style="margin-left:-2px;margin-top:5px;color:#ffcc00;"></i></a>';}
 
-else {star ='<a href="#" onclick="favList(\''+ response[i][1]  +'\')" style="z-index:100;float:right;margin-right:5px;height:40px;width:40px;border:0;background-color:transparent;" class="button"><i class="pe-7s-star pe-2x" style="margin-left:-2px;margin-top:5px;"></i></a>';alert('you do not like this'); }
+else {star ='<a href="#" onclick="favList(\''+ response[i][1]  +'\')" style="z-index:100;float:right;margin-right:5px;height:40px;width:40px;border:0;background-color:transparent;" class="button"><i class="pe-7s-star pe-2x" style="margin-left:-2px;margin-top:5px;"></i></a>'; }
 }
-alert(star);
+}
+
 var str = response[i][4];
 var singlequote = str.replace(/'/g, "qqqq");
 //var description = "'" + singlequote.replace(/(\r\n|\n|\r)/gm,"") + "'";
