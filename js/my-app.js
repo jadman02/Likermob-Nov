ab369608903163731004feb7db695afa0fa28014
jadman02 <jadman02@hotmail.com>
2014-12-26 09:36:29 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -2501,7 +2501,7 @@ var post_id_list = [];
 for (i = 0; i < allEntries.length; i++) {post_id_list.push("999999"+ allEntries[i].post_id + "999999");}
 
 
-
+alert(post_id_list);
 domain = "getposts";data_send = "pages_list=" + post_id_list + "&user_id=" + uid + "&latitude=" + latitude + "&longitude=" + longitude;
 
 $$.getJSON('http://www.smilesavers.net.au/'+ domain +'.php?callback=?', ''+ data_send +'',function(response){
