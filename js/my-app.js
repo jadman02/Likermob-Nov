5724f78d8b7a991c83487e34dde6e22d49de9261
jadman02 <jadman02@hotmail.com>
2014-12-26 22:13:33 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -2600,7 +2600,7 @@ var uid = localStorage.getItem("uid");
 domain = "getdeal";data_send = "post_id=" + post_id;
 $$.getJSON('http://www.smilesavers.net.au/'+ domain +'.php?callback=?', ''+ data_send +'',function(response){
 var page_id = response[0][1];var title = response[0][3];
-var homecontentimage = 'http://smilesavers.net.au/images/compressed/'+response[0][1]+'_'+response[0][21]+'.jpg';
+//var homecontentimage = 'http://smilesavers.net.au/images/compressed/'+response[0][1]+'_'+response[0][21]+'.jpg';
 });	
 myApp.modal({
     title: 'title',
@@ -2626,7 +2626,7 @@ myApp.modal({
         text: '<i class="pe-7s-more pe-lg"></i>',
         close: false,
         onClick: function() {
-        	loadMore(\''+ page_id  +'\',\''+ homecontentimage  +'\',\''+ title  +'\',\''+ uid +'\',\''+ post_id  +'\');
+        	//loadMore(\''+ page_id  +'\',\''+ homecontentimage  +'\',\''+ title  +'\',\''+ uid +'\',\''+ post_id  +'\');
         }
       },
     ]
