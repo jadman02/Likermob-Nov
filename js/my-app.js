f519c1dd01af5191fcc81c8962c8d230b53b9ad4
jadman02 <jadman02@hotmail.com>
2014-12-26 17:03:16 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -2553,7 +2553,7 @@ var myList = myApp.virtualList('.list-block.virtual-list', {
                   '<div class="item-media" style="padding:5px;"><img src="http://smilesavers.net.au/images/compressed/'+item.page_id+'_'+item.photo+'.jpg" style="width:40px;max-height:40px;overflow:hidden;"/></div>' +
                   '<div class="item-inner virtual-inner">' +
                      '<div class="item-title-row">'+
-                         '<div class="item-title"><img src="http://graph.facebook.com/'+item.page_id+'/picture?width=20&height=20" style="border-radius:50%;max-width:10px;margin-right:20px;"/>Save 10% on Coffee every day of the week</div>' +
+                         '<div class="item-title"><img src="http://graph.facebook.com/'+item.page_id+'/picture?width=20&height=20" style="border-radius:50%;max-width:20px;margin-right:10px;"/>Save 10% on Coffee every day of the week</div>' +
                         '<div class="item-after"></div>'+
                     '</div>'+
                     '<div class="item-subtitle">'+ item.expiry +'</div>' +
