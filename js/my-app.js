87d95ac07a42e696c9a7b125c4cfaf4abe5ea6cd
jadman02 <jadman02@hotmail.com>
2014-12-27 12:46:15 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -2614,9 +2614,17 @@ var url = 'http://smilesavers.net.au/images/compressed/'+response[0][1]+'_'+resp
 	
 	myApp.modal({
     title: '<img src="http://graph.facebook.com/'+page_id+'/picture?width=30&height=30" style="border-radius:50%;max-width:30px;"/>',
-     text: '<div class="content-block modal-block" style="padding:0;margin:0;max-height:300px;overflow: scroll;overflow-x:hidden;"><div class="content-block-inner" style="background-color:transparent;"><div class="modal_inner" style="background-color:hsla(0, 0%, 100%, 0.8);"><div class="content-block-title">' + title + '</div>'+description+' gdfsdffsdsdfdsfdsf fdssd fsf sdfds f<div class="content-block-title">Terms</div>fasfkjasdjksahdjksadkjsadnsajkdnas</div><div class="list-block" style="margin-top:10px;margin-bottom:10px;"><ul><li><div class="item-content"><div class="item-media"><img src="http://graph.facebook.com/'+ uid +'/picture?type=small"></div><div class="item-inner"><div class="item-input"><textarea id="dealbox1" placeholder="What do you think?"></textarea><a href="#" class="button" onclick="$$(\"#dealbox1\").val();share(value,page_id,url,title);">Share</a></div></div></div></li></ul</div></div></div>',
-    
-     
+     text: '<div class="content-block modal-block" style="padding:0;margin:0;max-height:300px;overflow: scroll;overflow-x:hidden;"><div class="content-block-inner" style="background-color:transparent;"><div class="modal_inner" style="background-color:hsla(0, 0%, 100%, 0.8);">'+
+     '<div class="messages-date">Sunday, Feb 3 <span>11:58</span></div>'+ 
+'<div class="row">'+
+        '<div class="col-20">Deal</div>'+
+        '<div class="col-80"><div class="messages-date" style="float:left;margin:0;padding:0;"><span>Smilesavers</span></div><div style="background-color:#e5e5ea;padding:10px;padding-bottom:30px;border-radius:10px;margin-top:20px;"><div id="dealtitle" style="float:left;font-weight:500;">'+ title + ' fklasjdl ksadjskld jsakldjs akld the best in the world</div><br/><div style="clear:both;">'+description+'</div><div style="float:left;font-size:14px;clear:both;"><i class="pe-7s-clock pe-lg"></i> Expires in 6 days</div></div></div>'+
+'</div>'+
+'<div class="row">'+
+        '<div class="col-20">Terms</div>'+
+        '<div class="col-80"><div style="background-color:#e5e5ea;padding:10px;padding-bottom:30px;border-radius:10px;margin-top:20px;"><div id="dealtitle" style="float:left;font-weight:500;">'+ terms + ' fklasjdl ksadjskld jsakldjs akld the best in the world</div></div></div>'+
+'</div>'+
+     '</div></div></div>',
     buttons: [
       {
         text: '<i class="pe-7s-close pe-lg"></i>',
