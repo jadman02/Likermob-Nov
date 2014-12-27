92b3be4da522355fac55b48e2fac25f42030503d
jadman02 <jadman02@hotmail.com>
2014-12-27 12:25:45 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -2608,13 +2608,13 @@ function removelikelList(post_id){
 var uid = localStorage.getItem("uid");
 domain = "getdeal";data_send = "post_id=" + post_id;
 $$.getJSON('http://www.smilesavers.net.au/'+ domain +'.php?callback=?', ''+ data_send +'',function(response){
-var page_id = response[0][1];var title = response[0][3];
+var page_id = response[0][1];var title = response[0][3];var description = response[0][4];var terms = response[0][5];
 var url = 'http://smilesavers.net.au/images/compressed/'+response[0][1]+'_'+response[0][21]+'.jpg';
 
 	
 	myApp.modal({
     title: '<img src="http://graph.facebook.com/'+page_id+'/picture?width=30&height=30" style="border-radius:50%;max-width:30px;"/>',
-    text: '<div class="content-block modal-block" style="padding:0;margin:0;max-height:300px;overflow: scroll;overflow-x:hidden;"><div class="content-block-inner" style="background-color:transparent;"><div class="modal_inner" style="background-color:hsla(0, 0%, 100%, 0.8);">' + title + ' ajkdaskjbsajkdbsakj dbsa kjdb sajk dbsajk dbsajkdbsajkdbs ajkd bsajkdbsajkdbsaj kdasbdkjasbd jsdbasjdb asjdbasjd bsajkdbasjkdba skjdba sk jdb asjkdbasjkdba sj kdbasjkd bsa</div><div class="list-block" style="margin-top:10px;margin-bottom:10px;"><ul><li><div class="item-content"><div class="item-media"><img src="http://graph.facebook.com/'+ uid +'/picture?type=small"></div><div class="item-inner"><div class="item-input"><textarea id="dealbox1" placeholder="What do you think?"></textarea><a href="#" class="button" onclick="$$(\"#dealbox1\").val();share(value,page_id,url,title);">Share</a></div></div></div></li></ul</div></div></div>',
+    text: '<div class="content-block modal-block" style="padding:0;margin:0;max-height:300px;overflow: scroll;overflow-x:hidden;"><div class="content-block-inner" style="background-color:transparent;"><div class="modal_inner" style="background-color:hsla(0, 0%, 100%, 0.8);"><div class="content-block-title">' + title + '</div>'+description+' gdfsdffsdsdfdsfdsf fdssd fsf sdfds f<div class="content-block-title">Terms</div>fasfkjasdjksahdjksadkjsadnsajkdnas</div><div class="list-block" style="margin-top:10px;margin-bottom:10px;"><ul><li><div class="item-content"><div class="item-media"><img src="http://graph.facebook.com/'+ uid +'/picture?type=small"></div><div class="item-inner"><div class="item-input"><textarea id="dealbox1" placeholder="What do you think?"></textarea><a href="#" class="button" onclick="$$(\"#dealbox1\").val();share(value,page_id,url,title);">Share</a></div></div></div></li></ul</div></div></div>',
      
     buttons: [
       {
