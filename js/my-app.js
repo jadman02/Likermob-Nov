b3ff9bac6aae80344ef2cc3407663a6762d02a31
jadman02 <jadman02@hotmail.com>
2014-12-29 09:11:25 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -1036,7 +1036,7 @@ mainView.loadContent(
         '  <div class="navbar-inner" style="border-bottom: 1px solid #c4c4c4;">' +
         '    <div class="left"><a href="#" class="back link"><i class="icon icon-back"></i><span>Back</span></a></div>' +
         '<div class="center"><img src="http://graph.facebook.com/'+page_id+'/picture?width=30&height=30" style="border-radius:50%;width:30px;"/></div>'+
-        '    <div class="right"><div class="item-input" onclick="openMap();"><label class="label-switch"><input type="checkbox"><div class="checkbox"></div></label></div>' +
+        '    <div class="right business-right"></div>' +
         '  </div>' +
         '</div>' +
         '<div class="pages business">' +
@@ -1047,13 +1047,7 @@ mainView.loadContent(
         '    <div class="page-content cover-business" style="z-index: 1;">' +
         '      <div class="content-block" style="margin-top:-9px;">' +
         '        <div class="content-block-inner" style="background-color:rgba(255,255,255,.4);"">' +
-	      '<div class="list-block media-list" style="margin-top:0px;"><ul style="background-color:hsla(0, 0%, 100%, 0.8);"><div id="cover-div"></div><div id="info-here"></div></ul></div>' +
-	'<div class="list-block media-list"><ul style="background-color:hsla(0, 0%, 100%, 0.8);"><li style="background-color:#3b5998;color:white;"><div class="item-content" style="background-color:transparent"><div class="item-media"><i class="pe-7s-like2 pe-lg"></i></div><div class="item-inner" style="border:0;"><div class="item-title-row"><div class="item-title">Deals I Like</div><div id="after-liked" class="item-after"></div></div></div></div></li><div id="like-deals-here"></div></ul></div>' +
-
-	'<div class="list-block media-list"><ul style="background-color:hsla(0, 0%, 100%, 0.8);"><li style="background-color:#ff8000;color:white;"><div class="item-content"><div class="item-media"><i class="pe-7s-like2 pe-lg pe-rotate-180"></i></div><div class="item-inner" style="border:0;"><div class="item-title-row"><div class="item-title">Deals I Don\'t Like</div><div id="after-notliked" class="item-after"></div></div></div></div></li><div id="deals-here"></div></ul></div>' +
-
-
-        
+	      '<div class="list-block media-list" style="margin-top:0px;"><ul style="background-color:hsla(0, 0%, 100%, 0.8);"><div id="cover-div"></div><div id="info-here"><li style="background-color:#3b5998;color:white;"><div class="item-content" style="background-color:transparent"><div class="item-media"><i class="pe-7s-like2 pe-lg"></i></div><div class="item-inner" style="border:0;"><div class="item-title-row"><div class="item-title">Deals I Like</div><div id="after-liked" class="item-after"></div></div></div></div></li><div id="like-deals-here"><li style="background-color:#ff8000;color:white;"><div class="item-content"><div class="item-media"><i class="pe-7s-like2 pe-lg pe-rotate-180"></i></div><div class="item-inner" style="border:0;"><div class="item-title-row"><div class="item-title">Deals I Don\'t Like</div><div id="after-notliked" class="item-after"></div></div></div></div></li><div id="deals-here"></div></ul></div>' +
         '        </div>' +
         '      </div>' +
         '    </div>' +
@@ -1062,7 +1056,7 @@ mainView.loadContent(
     );
 
 
-$$( '#info-here' ).append('<li><div class="item-content"><div class="item-media"><i class="pe-7s-ribbon pe-2x"></i></div><div class="item-inner"><div class="item-title-row"><div class="item-title">'+ name +'</div><div class="item-after name_after"></div></div></div></div></li>');
+$$( '#info-here' ).append('<li><div class="item-content"><div class="item-media"><i class="pe-7s-ribbon pe-2x"></i></div><div class="item-inner"><div class="item-title">'+ name +'</div></div></div></li>');
 
  
 
@@ -1133,13 +1127,13 @@ $$( '#info-here' ).append('<li><a href="#" onclick="openMap();" class="item-cont
 }
 
 if (res[0][4]){
-$$( '.name_after' ).append('<a href="tel:'+res[0][4]+'" class="button external" style="border:0;width:30px;height:30px;margin-right:5px;"><i class="pe-7s-call pe-lg"></i></a>');
+$$( '.business-right' ).append('<a href="tel:'+res[0][4]+'" class="button external" style="border:0;width:30px;height:30px;margin-right:5px;"><i class="pe-7s-call pe-lg"></i></a>');
 }
 
 if (res[0][16]){
-$$( '.name_after' ).append('<a href="mailto:'+res[0][16]+'" class="button external" style="border:0;width:30px;height:30px;"><i class="pe-7s-mail pe-lg"></i></a>');
+$$( '.business-right' ).append('<a href="mailto:'+res[0][16]+'" class="button external" style="border:0;width:30px;height:30px;"><i class="pe-7s-mail pe-lg"></i></a>');
 }
-
+//onclick="openMap();"
 	
 $$.getJSON('https://graph.facebook.com/'+ page_id +'?fields=cover', function(response){
 	
