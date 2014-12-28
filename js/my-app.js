791f501d4318b7eae0b2528b75ea529640e5f13e
jadman02 <jadman02@hotmail.com>
2014-12-28 19:45:40 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -1054,9 +1054,9 @@ mainView.loadContent(
         '        <div class="content-block-inner" style="background-color:rgba(255,255,255,.4);"">' +
 	      '<div class="list-block media-list" style="margin-top:0px;"><ul><div id="cover-div"></div><div id="info-here"></div></ul></div>' +
 
-	'<div class="list-block media-list"><ul><li style="background-color:#3b5998;color:white;"><div class="item-content"><div class="item-inner" style="border:0;text-align:center;"><div class="item-title"><i class="pe-7s-like2 pe-lg"></i> Deals I Like</div></div></div></li><div id="like-deals-here"></div></ul></div>' +
+	'<div class="list-block media-list"><ul><li style="background-color:#3b5998;color:white;"><div class="item-content"><div class="item-media"><i class="pe-7s-like2 pe-lg"></i></div><div class="item-inner" style="border:0;text-align:center;"><div class="item-title">Deals I Like</div></div></div></li><div id="like-deals-here"></div></ul></div>' +
 
-	'<div class="list-block media-list"><ul><li style="background-color:#ff8000;color:white;"><div class="item-content"><div class="item-inner" style="border:0;text-align:center;"><div class="item-title"><i class="pe-7s-like2 pe-lg pe-rotate-180"></i> Deals I Don\'t Like</div></div></div></li><div id="deals-here"></div></ul></div>' +
+	'<div class="list-block media-list"><ul><li style="background-color:#ff8000;color:white;"><div class="item-content"><div class="item-media"><i class="pe-7s-like2 pe-lg pe-rotate-180"></i></div><div class="item-inner" style="border:0;text-align:center;"><div class="item-title">Deals I Don\'t Like</div></div></div></li><div id="deals-here"></div></ul></div>' +
 
         
         
