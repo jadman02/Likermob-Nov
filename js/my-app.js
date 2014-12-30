09dd7de85971bfdfe7b6b80b2c7607e7e1bbecb2
jadman02 <jadman02@hotmail.com>
2014-12-30 11:03:32 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -1038,7 +1038,7 @@ mainView.loadContent(
         '  <div class="navbar-inner" style="border-bottom: 1px solid #c4c4c4;">' +
         '    <div class="left"><a href="#" class="back link"><i class="icon icon-back"></i><span>Back</span></a></div>' +
         
-        '    <div class="right business-right"></div>' +
+        '    <div class="right business-right">'+staryellow + starblue+'</div>' +
         '  </div>' +
         '</div>' +
         '<div class="pages business">' +
@@ -1067,9 +1067,9 @@ if(favEntries == null) favEntries = [];
 var staryellow = '<i class="pe-7s-star pe-lg hide button" id="yellow_b" onclick="removefavList(\''+ page_id  +'\',9)" style="border:20px solid transparent;margin-top:-10px;color:#ffcc00;"></i>';
 var starblue = '<i class="pe-7s-star pe-lg button" id="blue_b" onclick="favList(\''+ page_id  +'\',9)" style="border:20px solid transparent;margin-top:-10px;"></i>';
 
-if(favEntries.length > 0){for (j = 0; j < favEntries.length; j++) {if (favEntries[j].page_id == page_id){staryellow = '<i class="pe-7s-star pe-lg button" id="yellow_b" onclick="removefavList(\''+  page_id  +'\',9)" style="border:0;color:#ffcc00;"></i>';starblue = '<i class="pe-7s-star pe-lg hide button" id="blue_b" onclick="favList(\''+ page_id  +'\',9)" style="border:0;"></i>';}}}
+if(favEntries.length > 0){for (j = 0; j < favEntries.length; j++) {if (favEntries[j].page_id == page_id){staryellow = '<i class="pe-7s-star pe-lg button" id="yellow_b" onclick="removefavList(\''+  page_id  +'\',9)" style="border:0;color:#ffcc00;"></i>';starblue = '<i class="pe-7s-star pe-lg button" id="blue_b" onclick="favList(\''+ page_id  +'\',9)" style="border:0;"></i>';}}}
 
-$$( '#info-here' ).append('<li><div class="item-content"><div class="item-media"><img src="http://graph.facebook.com/'+page_id+'/picture?width=30&height=30" style="border-radius:50%;width:30px;"/></div><div class="item-inner"><div class="item-title-row"><div class="item-title">'+ name +'</div><div class="item-after name_after">'+staryellow + starblue+'</div></div></div></div></li>');
+$$( '#info-here' ).append('<li><div class="item-content"><div class="item-media"><img src="http://graph.facebook.com/'+page_id+'/picture?width=30&height=30" style="border-radius:50%;width:30px;"/></div><div class="item-inner"><div class="item-title-row"><div class="item-title">'+ name +'</div><div class="item-after name_after"></div></div></div></div></li>');
 
 
 
