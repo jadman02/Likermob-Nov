9c69cbb70f8a04c375addac5c6add5eb9451c632
jadman02 <jadman02@hotmail.com>
2015-01-03 12:07:16 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -1319,31 +1319,14 @@ function initialize(latitude,longitude,page_id) {
   var myLatlng = new google.maps.LatLng(latitude,longitude);
   var mapOptions = {
     zoom: 15,
-    center: myLatlng,
-    icon: 'http://graph.facebook.com/'+page_id+'/picture?width=30&height=30"'
+    center: myLatlng
   }
   var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
-var contentString = '<div id="content">'+
-      '<div id="siteNotice">'+
-      '</div>'+
-      '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
-      '<div id="bodyContent">'+
-      '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-      'sandstone rock formation in the southern part of the '+
-      'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-      'south west of the nearest large town, Alice Springs; 450&#160;km '+
-      '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-      'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-      'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-      'Aboriginal people of the area. It has many springs, waterholes, '+
-      'rock caves and ancient paintings. Uluru is listed as a World '+
-      'Heritage Site.</p>'+
-      '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-      'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-      '(last visited June 22, 2009).</p>'+
-      '</div>'+
+var contentString = '<div style="max-width:206px;color:#2c2c2c;padding-bottom:5px;"><p style="margin-top:0px;padding-top:0px;"><span style="font-weight:400;">S</span><br/><span style="font-weight:300;">S</span></p>' +
+      '<p><a href="#">Website</a></br>Phone</p>'+
       '</div>';
+
 
   var infowindow = new google.maps.InfoWindow({
       content: contentString
@@ -1352,11 +1335,10 @@ var contentString = '<div id="content">'+
   var marker = new google.maps.Marker({
       position: myLatlng,
       map: map,
-      title: 'Hello World!'
+      title: 'Hello World!',
+    icon: 'http://graph.facebook.com/'+page_id+'/picture?width=30&height=30"'
   });
-  google.maps.event.addListener(marker, 'click', function() {
-    infowindow.open(map,marker);
-  });
+infowindow.open(map,marker);
 }
 
 
