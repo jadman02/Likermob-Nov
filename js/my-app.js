59590e72369c922f5d318f2d4639e634dddbcbaf
jadman02 <jadman02@hotmail.com>
2015-01-04 15:34:58 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -1112,7 +1112,7 @@ mainView.loadContent(
         '  <div data-page="business" class="page business no-navbar">' +
         '    <!-- Scrollable page content-->' +
 '<div id="map-canvas"></div>' +
-        '    <div class="page-content cover-business" style="z-index: 1;" style="background-color:transparent;max-height:90%;">' +
+        '    <div class="page-content cover-business" style="z-index: 1;" style="background-color:transparent;">' +
         '      <div class="content-block" style="margin-top:-9px;">' +
         '        <div class="content-block-inner" style="background-color:rgba(255,255,255,.4);">' +
 	      '<div class="list-block media-list" style="margin-top:0px;">' +
