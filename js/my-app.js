317c44eff2f31a8f1e5b7d8bc31f467694d5d202
jadman02 <jadman02@hotmail.com>
2015-01-05 16:48:25 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -1124,7 +1124,7 @@ mainView.loadContent(
        ' <a href="#tab1" onclick="openLikez()" class="tab-link active">' +
            '<span class="badge blue-circle"><i class="pe-7s-refresh pe-spin pe-lg" style="color:#3b5998" id="refresh-like"></i><div id="after-liked"></div></span>' +
         '</a>' +
-       '<div class="tab-link">  <a href="#" id="callicon">' +
+       '<div class="tab-link" style="min-width:25%;">  <a href="#" id="callicon">' +
            ' <i class="pe-7s-call pe-lg"></i>' +
        ' </a>' +
        '<div id="call-right"></div></div>'+
