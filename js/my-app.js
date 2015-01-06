0cf018a2ca40e2a69a8cc42dd70dcae9f70e5f7d
jadman02 <jadman02@hotmail.com>
2015-01-06 11:54:34 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -1241,7 +1241,7 @@ var allEntries = JSON.parse(localStorage.getItem("allEntries"));
 for (k = 0; k < allEntries.length; k++) { 
 if (allEntries[k].page_id == page_id){	
 deals_liked ++;
-if ((deals_liked == 1) && (allEntries[k].latitude) && (allEntries[k].longitude)) {alert('allEntries[k].latitude');}
+if ((deals_liked == 1) && (allEntries[k].latitude) && (allEntries[k].longitude)) {initialize(allEntries[k].latitude,allEntries[k].longitude);}
 var unix = allEntries[k].expiry + offset;
 var d = new Date(unix);
 var month_name = month[d.getUTCMonth()];
