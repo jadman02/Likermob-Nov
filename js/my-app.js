1ad73fc55842e1a60d99c66fb51a06311f980ffb
jadman02 <jadman02@hotmail.com>
2015-01-11 20:07:33 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -2937,5 +2937,5 @@ function shareDeal(page_id,url,title){
 	
 }
 
-function favOn(){$$( "#whitehomestar" ).hide();$$( "#yellowhomestar" ).show();$$( '#yellowhomestar' ).css( 'margin-top', '10px');}
-function favOff(){$$( "#whitehomestar" ).show();$$( "#yellowhomestar" ).hide();$$( '#whitehomestar' ).css( 'margin-top', '10px');}
+function favOn(){$$( "#whitehomestar" ).hide();$$( "#yellowhomestar" ).show();$$( '#yellowhomestar' ).css( 'padding-top', '10px');}
+function favOff(){$$( "#whitehomestar" ).show();$$( "#yellowhomestar" ).hide();$$( '#whitehomestar' ).css( 'padding-top', '10px');}
