ce80f4b95bfc10e2c416c3be23c9f96c4ddebdcc
jadman02 <jadman02@hotmail.com>
2015-01-13 18:27:02 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -313,8 +313,7 @@ if (type=='like') {$$( '#result' ).append('<li class="swipeout full s_'+ respons
 '<div class="imagediv">'+
 
 
-'<div style="min-height:40px;background-color:hsla(0, 0%, 0%, 0.7);margin:0 auto;text-align:center;color:white;"><div style="width:250px;margin:0 auto;">'+ response[i][3] + ' fklasjdl ksadjskld jsakldjs akld the best in the world'+
-
+'<div style="height:40px;background-color:hsla(0, 0%, 0%, 0.7);margin:0 auto;text-align:center;"><div style="width:250px;margin:0 auto;">'+
 '<a href="#" onclick="loadMore(\''+ response[i][1]  +'\',\''+ homecontentimage  +'\',\''+ response[i][3]  +'\',\''+ uid +'\',\''+ response[i][2]  +'\');" style="z-index:100;float:right;margin-right:5px;height:40px;width:40px;border:0;background-color:transparent;" class="button"><i class="pe-7s-more pe-2x" style="margin-left:-5px;margin-top:5px;"></i></a>'+
 staryellow +
 starblue +
