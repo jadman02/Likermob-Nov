515a48a1febe25efca30ac0491fc554c32f7101f
jadman02 <jadman02@hotmail.com>
2015-01-13 23:08:40 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -2222,7 +2222,7 @@ function moveHomeimage(){
 
 	var resultheight = liHeight + height_image;
      $$( '.imagediv' ).css( 'padding-top', positionimage + 'px' );
-     $$( '#result' ).css( 'height', resultheight + 'px' );
+     //$$( '#result' ).css( 'height', resultheight + 'px' );
 
  };
 }
