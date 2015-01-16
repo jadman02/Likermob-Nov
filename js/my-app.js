c15f253fc26e5c1b82cb6c8225333273d8e5450f
jadman02 <jadman02@hotmail.com>
2015-01-16 12:16:32 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -345,8 +345,7 @@ if (type=='like') {$$( '#result' ).append('<li class="swipeout full s_'+ respons
 '<div id="commentlist"></div>'+
 '<div class="row">'+
 '<a href="#" id="commentloader" class="button hide" style="border:0;height:40px;width:40px;margin:0 auto;background-color:transparent;margin-top:20px;"><i class="pe-7s-refresh pe-spin pe-2x"></i></a>'+
-'<div style="width:80px;margin:0 auto;"><a href="#" id="chatloader" onclick="getComments();" style="z-index:100;height:40px;width:40px;float:left;border-radius:50%;background-color:transparent;margin-top:20px;" class="button"><i class="pe-7s-chat pe-2x" style="margin-left:-2px;margin-top:5px;"></i></a>'+
-'<a href="#" id="chatloader" onclick="getComments();" style="z-index:100;height:40px;width:40px;float:left;border-radius:50%;background-color:transparent;margin-top:20px;" class="button"><i class="pe-7s-comment pe-2x" style="margin-left:-2px;margin-top:5px;"></i></a></div></div>'+  
+'<a href="#" id="chatloader" onclick="getComments();" style="z-index:100;height:40px;width:40px;margin:0 auto;border-radius:50%;background-color:transparent;margin-top:20px;" class="button"><i class="pe-7s-chat pe-2x" style="margin-left:-2px;margin-top:5px;"></i></a></div>'+  
 
 '</div>'+    
         
