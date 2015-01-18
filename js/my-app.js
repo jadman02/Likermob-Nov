6bc3042a8c7b4beff3a1a9aed129df01a6351887
jadman02 <jadman02@hotmail.com>
2015-01-18 22:55:56 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -18,8 +18,8 @@ myApp.onPageBeforeInit('index', function (page) {
 
 var uid = localStorage.getItem("uid");
 
-var position = localStorage.getItem("position");
-$$('#fulladdressf').attr("placeholder",position);
+//var position = localStorage.getItem("position");
+//$$('#fulladdressf').attr("placeholder",position);
 
 document.getElementById("profilepic").innerHTML = '<img src="http://graph.facebook.com/' + uid + '/picture?type=normal" style="margin:0 auto;text-align:center;width:80px;border-radius:50%;"/>';
 
@@ -1960,7 +1960,13 @@ var position = localStorage.getItem("position");
 '<div class="slider-custom"><div class="slider-container slider-init"><div class="slider-wrapper">'+
         '<div class="slider-slide">'+
         
-
+                     ' <form class="searchbar" style="background-color:#ff8000; border:0;">'+
+       ' <div class="searchbar-input">'+
+          '  <input type="search" style="float:left;" placeholder="'+ position +'" onkeyup="searchPlaces(\'set\');" id="fulladdressf">'+
+       ' <a href="#" id="resultfclear" onclick="clearInput();" style="display:none;float:left;margin-left:-25px;margin-top:3px;color:hsl(0, 0%, 70%);"><i class="pe-7s-close-circle pe-lg"></i></a></div>'+
+'<div class="item-title label" style="background-color:white;border-radius:5px;margin-left:5px;padding:4px;float:left;width:60px;"><output name="amount" id="amount" for="rangeInput">20</output> km</div>'+
+//'<a href="#" class="button" style="margin-left:5px;color:white;border:0;"><i class="pe-7s-signal pe-lg"></i></a>'+
+   ' </form>'+
 
 
 '<div class="content-block">'+
