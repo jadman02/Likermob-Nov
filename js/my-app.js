7a423a5eddfc548f4bac686ff9e6abbf0acea688
jadman02 <jadman02@hotmail.com>
2015-01-24 15:46:00 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -3087,7 +3087,7 @@ function videoHelp(){
 	
 myApp.modal({
     title:  'How Likermob Works',
-    text: '<iframe width="560" height="315" src="//www.youtube.com/embed/4SNyPrGnT9U" frameborder="0" allowfullscreen></iframe>',
+    text: '<iframe width="560" height="315" src="http://www.youtube.com/embed/4SNyPrGnT9U" frameborder="0" allowfullscreen></iframe>',
    buttons: [
       {
         text: 'Cancel',
