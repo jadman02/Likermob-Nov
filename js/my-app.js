17eba3d7965965bf036fd72490cffa5cc8ceec10
jadman02 <jadman02@hotmail.com>
2015-01-25 14:36:52 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -382,7 +382,7 @@ if (type=='like') {$$( '#result' ).append('<li class="swipeout full s_'+ respons
 
    '<div class="content-block" id="content-block" style="clear:both;">'+
      ' <div class="content-block-inner" style="height:100%;padding-bottom:20px;background-color:hsla(0, 0%, 100%, 0.8);">'+
-'<span class="badge" style="clear:both;float:right;margin:0 auto;margin-left:5px;margin-top:-30px;background-color:#ff8000;"><i class="pe-7s-like2 pe-rotate-180" style="margin-right:2px;"></i>'+ response[i][18]  +'</span><span class="badge" style="float:right;margin:0 auto;margin-top:-30px;background-color:#3b5998;"><i class="pe-7s-like2" style="margin-right:2px;"></i>'+ response[i][17]  +'</span>'+
+'<span class="badge" style="clear:both;float:right;margin:0 auto;margin-left:5px;margin-top:-40px;background-color:#ff8000;"><i class="pe-7s-like2 pe-rotate-180" style="margin-right:2px;"></i>'+ response[i][18]  +'</span><span class="badge" style="float:right;margin:0 auto;margin-top:-40px;background-color:#3b5998;"><i class="pe-7s-like2" style="margin-right:2px;"></i>'+ response[i][17]  +'</span>'+
 
 
 
