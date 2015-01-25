d917a24f3adbe4c59badf96d7454319aa7a949d9
jadman02 <jadman02@hotmail.com>
2015-01-25 16:15:06 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -2938,10 +2938,10 @@ myApp.modal({
         '<div style="background-color:transparent;padding:10px;width:100%;text-align:left;"><div style="float:left;font-weight:500;margin-top:20px;">Terms</div><br/><div style="clear:both;float:none;"><div id="termsdiv"></div></div></div>'+
 '</div></div>'+
      
-     '<div id="commentlist"></div>'+
+     '<div id="commentlist1"></div>'+
 '<div class="row" style="margin-bottom:10px;">'+
-'<a href="#" id="commentloader" class="button hide" style="border:0;height:40px;width:40px;margin:0 auto;background-color:transparent;margin-top:20px;"><i class="pe-7s-refresh pe-spin pe-2x"></i></a>'+
-'<a href="#" id="chatloader" onclick="getComments();" style="z-index:100;height:40px;width:40px;margin:0 auto;border-radius:50%;background-color:transparent;margin-top:20px;" class="button"><i class="pe-7s-chat pe-2x" style="margin-left:-2px;margin-top:5px;"></i></a></div>'+  
+'<a href="#" id="commentloader1" class="button hide" style="border:0;height:40px;width:40px;margin:0 auto;background-color:transparent;margin-top:20px;"><i class="pe-7s-refresh pe-spin pe-2x"></i></a>'+
+'<a href="#" id="chatloader1" onclick="getComments(\'popup\');" style="z-index:100;height:40px;width:40px;margin:0 auto;border-radius:50%;background-color:transparent;margin-top:20px;" class="button"><i class="pe-7s-chat pe-2x" style="margin-left:-2px;margin-top:5px;"></i></a></div>'+  
 
  
      
