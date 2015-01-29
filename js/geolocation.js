0bc53ecb506575981e7083cf15e5aa8cbf55418b
jadman02 <jadman02@hotmail.com>
2015-01-29 18:09:17 +1100

    Update geolocation.js

--- js/geolocation.js
+++ js/geolocation.js
@@ -6,18 +6,21 @@ function getmyLocation(){
     // onSuccess Geolocation
     //
     function onSuccess(position) {
-localStorage.setItem("latitude", position.coords.latitude);
-localStorage.setItem("longitude", position.coords.longitude);
+alert(position.coords.latitude);
+alert(position.coords.longitude);
+alert(position.timestamp);
 
-var datetoday1 = new Date();
-var offset1 = datetoday1.getTimezoneOffset() * 60;
-var timeGPS = position.timestamp + offset1;
-alert(timeGPS);
+//localStorage.setItem("latitude", position.coords.latitude);
+//localStorage.setItem("longitude", position.coords.longitude);
+//var datetoday1 = new Date();
+//var offset1 = datetoday1.getTimezoneOffset() * 60;
+//var timeGPS = position.timestamp + offset1;
+//alert(timeGPS);
+
 //currentdate.getHours() + ':' + currentdate.getSeconds(); + '(' + currentdate.getDate() + '/' + (currentdate.getMonth()+1)  + '/' + currentdate.getFullYear() + ')';
 localStorage.setItem("formatted_address", timeGPS);
-var formatted_address_placeholder = localStorage.getItem("formatted_address");
-if (formatted_address_placeholder != 'null'){$$('#fulladdressf').attr("placeholder",formatted_address_placeholder);document.getElementById("fulladdressf").value = formatted_address_placeholder;}
-    mainView.router.loadPage('index.html');
+$$('#fulladdressf').attr("placeholder",formatted_address_placeholder);document.getElementById("fulladdressf").value = formatted_address_placeholder;
+mainView.router.loadPage('index.html');
         
     }
 
