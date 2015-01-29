8c1015a46333b18eed5548bf66f72cdddb154a50
jadman02 <jadman02@hotmail.com>
2015-01-29 22:01:13 +1100

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -643,31 +643,6 @@ document.getElementById("after-notliked2").innerHTML = dislikecount;
 document.getElementById("after-notliked2").value = dislikecount;
 }
 
-
-
-
-function askLocation() {
-	
-	navigator.geolocation.getCurrentPosition(onSuccess, onError);
-}
-
-    // onSuccess Geolocation
-    //
-    function onSuccess(position) {
-        //var element = document.getElementById('geolocation');
-        //element.innerHTML = 'Latitude: '           + position.coords.latitude              + '<br />' +
-                            //'Longitude: '          + position.coords.longitude             + '<br />' +
-                            //'Accuracy: '           + position.coords.accuracy              + '<br />' +
-                            //'Timestamp: '          +                                   position.timestamp          + '<br />';
-    }
-
-    // onError Callback receives a PositionError object
-    //
-    function onError(error) {
-        alert('code: '    + error.code    + '\n' +
-                'message: ' + error.message + '\n');
-    }
-
 function saveCategory(id){
 	
 localStorage.setItem("category_filter", id);	
