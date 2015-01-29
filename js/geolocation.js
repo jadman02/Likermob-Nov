9c9d01a69ce7670b28cbffbba651db1932ca8518
jadman02 <jadman02@hotmail.com>
2015-01-29 17:20:52 +1100

    Update geolocation.js

--- js/geolocation.js
+++ js/geolocation.js
@@ -1,27 +1,32 @@
-// Wait for Cordova to load
-    //
-   document.addEventListener("deviceready", onDeviceReady, false);
-
-    // Cordova is ready
-    //
-    function onDeviceReady() {
-        navigator.geolocation.getCurrentPosition(onSuccess, onError);
-       
-       
-    }
+function getmyLocation(){
+   navigator.geolocation.getCurrentPosition(onSuccess, onError); 
+    
+}
 
     // onSuccess Geolocation
     //
     function onSuccess(position) {
         var element = document.getElementById('geolocation');
-        element.innerHTML = 'Latitude: '           + position.coords.latitude              + '<br />' +
-                            'Longitude: '          + position.coords.longitude             + '<br />' +
+        element.innerHTML = 'Latitude: '           +               + '<br />' +
+                            'Longitude: '          +              + '<br />' +
                             'Altitude: '           + position.coords.altitude              + '<br />' +
                             'Accuracy: '           + position.coords.accuracy              + '<br />' +
                             'Altitude Accuracy: '  + position.coords.altitudeAccuracy      + '<br />' +
                             'Heading: '            + position.coords.heading               + '<br />' +
                             'Speed: '              + position.coords.speed                 + '<br />' +
-                            'Timestamp: '          +                                   position.timestamp          + '<br />';
+                            'Timestamp: '          +                                             + '<br />';
+localStorage.setItem("latitude", position.coords.latitude);
+localStorage.setItem("longitude", position.coords.longitude);
+var offset = datetoday.getTimezoneOffset() * 60;
+var timeGPS = position.timestamp + offset;
+alert(timeGPS);
+//currentdate.getHours() + ':' + currentdate.getSeconds(); + '(' + currentdate.getDate() + '/' + (currentdate.getMonth()+1)  + '/' + currentdate.getFullYear() + ')';
+localStorage.setItem("formatted_address", timeGPS);
+var formatted_address_placeholder = localStorage.getItem("formatted_address");
+if (formatted_address_placeholder != 'null'){$$('#fulladdressf').attr("placeholder",formatted_address_placeholder);document.getElementById("fulladdressf").value = formatted_address_placeholder;}
+functionEmpty();
+    mainView.router.loadPage('index.html');
+        
     }
 
     // onError Callback receives a PositionError object
