57fbf66795503ae36e274c76156ac983934702f3
jadman02 <jadman02@hotmail.com>
2015-02-02 08:14:06 +1100

    Update geolocation.js

--- js/geolocation.js
+++ js/geolocation.js
@@ -60,7 +60,8 @@ $$('#fulladdressf').attr("placeholder",timeGPS);
 
 localStorage.setItem("latitude", position.coords.latitude);
 localStorage.setItem("longitude", position.coords.longitude);
-var timestampy = position.timestamp.substring(0, 10);
+var timestamp = position.timestamp;
+var timestampy = timestamp.substring(0, 10);
 alert(timestampy);
 var datetoday1 = new Date();
 var offset1 = datetoday1.getTimezoneOffset() * 60;
