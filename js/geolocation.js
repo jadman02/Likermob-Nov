31c5ef0f28504f19a3cc4e6c9163d4de63365aef
jadman02 <jadman02@hotmail.com>
2015-02-02 11:34:16 +1100

    Update geolocation.js

--- js/geolocation.js
+++ js/geolocation.js
@@ -69,10 +69,10 @@ var town = extractFromAdress(response.results[0].address_components, "locality")
 var state = extractFromAdressShort(response.results[0].address_components, "administrative_area_level_1");
 var country = extractFromAdress(response.results[0].address_components, "country");
 
-alert(town + ' ' + state + ' ' + zip + ', ' + country);
 
-//var formatted_address_placeholder = ;
 
+var formatted_address_placeholder = town + ' ' + state + ' ' + zip + ', ' + country;
+alert(formatted_address_placeholder);
 //localStorage.setItem("formatted_address", ;
 //$$('#fulladdressf').attr("placeholder", town + ' ' + state + ' ' + zip + ', ' + country);
 $$('#formatted_address_time').append('Updated 2/2/15');
